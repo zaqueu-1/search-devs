8c99580b4f5c52cec2dfdf6c342f8af6f4780397
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2024-01-15 14:37:50 -0300

    Home page initial thoughts

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (search) => {
+    console.log(search)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='home-container'>
+      <h1>
+        <span style={{color:'#0069CA'}}>Search</span>
+        <span style={{color:'#8C19D2'}}> d_evs</span>
+      </h1>
+      <div className='input-box'>
+        <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+        <svg className='searchIcon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18.031 16.617L22.314 20.899L20.899 22.314L16.617 18.031C15.0237 19.3082 13.042 20.0029 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20.0029 13.042 19.3082 15.0237 18.031 16.617ZM16.025 15.875C17.2941 14.5699 18.0029 12.8204 18 11C18 7.132 14.867 4 11 4C7.132 4 4 7.132 4 11C4 14.867 7.132 18 11 18C12.8204 18.0029 14.5699 17.2941 15.875 16.025L16.025 15.875Z" fill="#A0AEC0"/>
+        </svg>
+        <button onClick={handleSearch(search)}>Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/Users.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Users.css'
+
+function Users() {
+
+
+  return (
+    <div className='users-container'>
+
+    </div>
+  )
+}
+
+export default Users
