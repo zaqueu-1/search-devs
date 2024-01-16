405414280dc17e5485f201ff497b85d6afad076a
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2024-01-16 11:04:25 -0300

    Added Hash History in place of old Browser History

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { createHashHistory } from 'history'
 import Home from './routes/Home.jsx'
 import User from './routes/User.jsx'
 import ErrorPage from './routes/ErrorPage.jsx'
 import './index.css'
 import { DarkProvider } from './contexts/darkMode/DarkContext.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/users/:username",
-    element: <User />,
-  }
-])
+const history = createHashHistory()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DarkProvider>
-      <RouterProvider router={router} />
+      <BrowserRouter history={history}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/:username" element={<User />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </DarkProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
